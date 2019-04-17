3f9abac152a4c50744b105ae40ed23f475d0e228
ijmcnally <mcnallyi@mit.edu>
2019-04-17 16:20:02 -0400

    chord diagram upload

---
+++ ChordDiagram.py
@@ -0,0 +1,124 @@
+
+import sys
+sys.path.append('..')
+from common.core import *
+from common.audio import *
+from common.mixer import *
+from common.wavegen import *
+from common.wavesrc import *
+from common.gfxutil import *
+
+from kivy.graphics.instructions import InstructionGroup
+from kivy.graphics import Color, Ellipse, Line, Rectangle
+from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
+from kivy.clock import Clock as kivyClock
+from kivy.uix.floatlayout import FloatLayout
+
+import random
+import numpy as np
+import bisect
+
+class MainWidget(BaseWidget) :
+    def __init__(self):
+        super(MainWidget, self).__init__()
+
+        self.diagram = ChordDiagram()
+        self.canvas.add(self.diagram)
+
+    def on_update(self):
+    	pass
+
+
+class ChordDiagram(InstructionGroup):
+
+	Chords = {'G':[3,2,0,0,0,3],
+			'am':[-1,0,2,2,1,0],
+			'bm':[-1,2,4,4,3,2],
+			'C':[-1,3,2,0,1,0],
+			'D':[-1,-1,0,2,3,2],
+			'D7':[-1,-1,0,2,1,2],
+			'em':[0,2,2,0,0,0]}
+
+	def __init__(self, size=400, pos=(100,100), chord='em'):
+		super(ChordDiagram, self).__init__()
+		self.size = size
+		self.x, self.y = pos
+		
+		# fretboard
+		x = self.x + self.size * .1
+		y1 = self.y + self.size * .2
+		y2 = self.y + self.size * .8
+
+		fretboard = InstructionGroup()
+		fretboard.add(Color(139/255,69/255,19/255))
+		background = Rectangle(pos=(x, y1), size=(self.size * 1.4, self.size * .6))
+		fretboard.add(background)
+		self.add(fretboard)
+
+		# frets
+		last_x = x
+		self.finger_placements = [x]
+		scale_length = self.size * 6
+		print(scale_length)
+		for i in range(4):
+			fret = InstructionGroup()
+			fret.add(Color(238/255, 238/255, 224/255))
+			x = last_x + scale_length/17.817
+			self.finger_placements.append((last_x + x)/2)
+			last_x = x
+			line = Line(points=[x, y1, x, y2], width=self.size/100, cap='round')
+			fret.add(line)
+			self.add(fret)
+			scale_length = scale_length - scale_length/17.817
+
+		# fret marker
+		x_dot = self.finger_placements[3]
+		dot = InstructionGroup()
+		dot.add(Color(1,1,1))
+		circle = CEllipse(cpos=(x_dot, self.y + self.size/2), csize=(self.size*.06, self.size*.06))
+		dot.add(circle)
+		self.add(dot)
+
+		# strings
+		self.string_heights = []
+		for i in range(6):
+			string = InstructionGroup()
+			if i < 3:	# brass strings
+				string.add(Color(181/255, 166/255, 66/255))
+				width = size/100
+			else:
+				string.add(Color(180/255, 180/255, 180/255))
+				width = size/200
+			y = self.y + self.size * (.25 + .1 * i)
+			self.string_heights.append(y)
+			line = Line(points=[self.x + self.size * .1, y, self.x + self.size * 1.5, y], width=width, cap='round')
+			string.add(line)
+			self.add(string)
+
+		# nut
+		x = self.x + self.size * .1
+		nut = InstructionGroup()
+		nut.add(Color(238/255, 238/255, 224/255))
+		line = Line(points=[x, y1, x, y2], width=self.size/50, cap='square')
+		nut.add(line)
+		self.add(nut)
+
+		# chord fingering
+		indices = self.Chords[chord]
+		for index, fret in enumerate(indices):
+			
+			# TO DO: handle muted strings
+			if fret == -1:
+				pass
+			
+			finger = InstructionGroup()
+			finger.add(Color(50/255,1,0))
+			dot = CEllipse(cpos=(self.finger_placements[fret], self.string_heights[index]), csize=(self.size*.1, self.size*.1))
+			finger.add(dot)
+			self.add(finger)
+
+
+
+
+
+run(MainWidget)
