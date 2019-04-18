1b8e56b8b92a5b38599172136591ad58798b2b71
Angel Lopez <angellop@mit.edu>
2019-04-18 18:15:41 -0400

    Added MIDI reading from usb input

---
+++ MIDIlistener.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#
+# test_midiin_poll.py
+#
+"""Show how to receive MIDI input by polling an input port."""
+
+from __future__ import print_function
+
+import logging
+import sys
+import time
+
+from rtmidi.midiutil import open_midiinput
+
+class MIDIInput(object):
+    def __init__(self, port = 0):
+
+        self.last_note = 0
+        self.current_notes = set()
+
+
+        try:
+            self.midiin, port_name = open_midiinput(port)
+        except:
+            sys.exit()
+
+    def on_update(self):
+        #verify if the port is receiving any input
+        try:
+            timer = time.time()
+            #get the MIDI input
+            msg = self.midiin.get_message()
+
+            #if input received
+            if msg:
+                message, deltatime = msg
+                timer += deltatime
+
+                #separate message into Note and velocity
+                midiNote = message[1]
+                velocity = message[2]
+                print(midiNote, velocity)
+                if velocity != 0:
+                    self.current_notes.add(midiNote)
+                    self.last_note = midiNote
+                else:
+                    if midiNote in self.current_notes:
+                        self.current_notes.remove(midiNote)
+
+            time.sleep(0.01)
+        except KeyboardInterrupt:
+            print('')
+        
+
+
+
+
+
+
+
+# Prompts user for MIDI input port, unless a valid port number or name
+# is given as the first argument on the command line.
+# API backend defaults to ALSA on Linux.
+
+
+
+
+# port = sys.argv[1] if len(sys.argv) > 1 else None
+
+# try:
+#     midiin, port_name = open_midiinput(port)
+# except (EOFError, KeyboardInterrupt):
+#     sys.exit()
+
+# print("Entering main loop. Press Control-C to exit.")
+# try:
+#     timer = time.time()
+#     while True:
+#         msg = midiin.get_message()
+
+#         if msg:
+#             message, deltatime = msg
+#             timer += deltatime
+#             # print("[%s] @%0.6f %r" % (port_name, timer, message))
+#             print(message)
+
+#         time.sleep(0.01)
+# except KeyboardInterrupt:
+#     print('')
+# finally:
+#     print("Exit.")
+#     midiin.close_port()
+#     del midiin
+
+
+
+
+
+
+#fp.py
+
+
+
+
+        # frame = self.controller.on_update()
+        # self.time = frame / 44100
+        # self.display.on_update(self.time)
+        # self.player.on_update(self.time)
+
+        # if not self.player.get_done():
+        #     self.label.text = "Press \"P\" to "
+        #     if self.playing:
+        #         self.label.text += "pause.\n"
+        #     elif self.started:
+        #         self.label.text += "unpause\n"
+        #         self.label.text += "Press \"R\" to restart\n"
+
+        #     else:
+        #         self.label.text += "begin.\n"
+        #     self.label.text += "score: %d\n" % self.player.get_score()
+        #     if self.player.get_streak() >= 5:
+        #         self.label.text += "                                                  Streak: %d   2x Bonus" % self.player.get_streak()
+        # else:
+        #     self.label.text = "Final score is: %d\n" % self.player.get_score()
+        #     self.label.text += "Accuracy is: %d %%\n" % self.player.get_accuracy()
+
+        #     self.label.text += "Highest streak: %d\n" % self.player.get_max_streak()
+        #     self.label.text += "Press \"R\" to restart"
+
+
---
+++ MIDItest.py
@@ -0,0 +1,37 @@
+from common.core import *
+from common.gfxutil import *
+
+from Audio import AudioController
+from Display import *
+from Player import Player
+from SongData import SongData
+
+
+from MIDIlistener import MIDIInput
+vel = 200
+nowbar_height = 100
+color_mapping = {1:(1,0,0), 2:(1,1,0), 3: (0,1,0), 4: (0,1,1), 5:(0,0,1)}
+
+class MainWidget(BaseWidget) :
+    def __init__(self):
+        super(MainWidget, self).__init__()
+        #creates MIDI listener object
+        self.MIDI = MIDIInput(1)
+
+        self.label = topleft_label()
+        self.add_widget(self.label)
+
+    def on_key_down(self, keycode, modifiers):
+        pass
+        
+
+
+    def on_key_up(self, keycode):
+        pass
+        
+    def on_update(self) :
+        self.label.text = str(self.MIDI.last_note) + '\n'
+        self.label.text += str(self.MIDI.current_notes)
+        self.MIDI.on_update()
+
+run(MainWidget)
