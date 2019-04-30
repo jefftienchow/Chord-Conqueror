534dd76b74849f11dbe3fee4a10e1528ba5ba193
Angel Lopez <angellop@mit.edu>
2019-04-30 16:59:33 -0400

    added progressbar cleanup

--- ProgressBar.py
+++ ProgressBar.py
@@ -10,24 +10,34 @@ class ProgressBar(InstructionGroup):
         self.start_time = sections[start][0]
         self.end_time = sections[end][0]
         self.duration = self.end_time - self.start_time
-
+        self.objects = []
         self.color_mapping = color_mapping
 
         x_loc = 50
         for i in range(start, end):
             print(sections[i][1])
             color = self.color_mapping[sections[i][1]]
-            self.add(Color(*color))
+            color = Color(*color)
+            self.add(color)
+            self.objects.append(color)
             length = (sections[i+1][0] - sections[i][0]) * 700 / self.duration
             rectangle = Rectangle(pos=(x_loc, 500), size = (length, 50))
             self.add(rectangle)
+            self.objects.append(rectangle)
             x_loc += length
-
-        self.add(Color(1,1,1))
+        newcolor = Color(1,1,1)
+        self.add(newcolor)
+        self.objects.append(newcolor)
         self.cursor = Rectangle(pos = (50, 500), size = (2, 50))
+
         self.add(self.cursor)
+        self.objects.append(self.cursor)
 
     def set_cursor(self, time):
         xpos = (time - self.start_time) * 700 / self.duration + 50
         self.cursor.pos = (xpos, 500)
+    def cleanup(self):
+        for obj in self.objects:
+            self.remove(obj)
+
 
